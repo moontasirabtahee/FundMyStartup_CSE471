4db029a4f6386c312e4be3d5b9ac6825f73f1a31
shahriar <ayon4000@gmail.com>
2023-08-11 14:56:20 +0600

    f

--- FundMyStartup/dashboard/models.py
+++ FundMyStartup/dashboard/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Idea(models.Model):
+    entrepreneur = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    Equity = models.TextField()
+    funding = models.DecimalField(max_digits=10, decimal_places=2)
+    Sales =  models.DecimalField(max_digits=10, decimal_places=2)
+    owner= models.TextField()
+    profit = models.DecimalField(max_digits=10, decimal_places=2)
+    debts = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
 
 # Create your models here.
--- FundMyStartup/dashboard/views.py
+++ FundMyStartup/dashboard/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Idea
 
-# Create your views here.
+
+def startup(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        Equity = request.POST['Equity']
+        funding = request.POST['funding']
+        Sales= request.POST['Sales']
+        owner= request.POST['Owner']
+        profit= request.POST['Profit']
+        Debts= request.POST['Debts']
+
+        # Create a new Idea instance and save it to the database
+        new_idea = Idea(title=title, Equity=Equity, funding=funding, Sales=Sales, owner=owner, profit=profit, Debts=Debts)
+        new_idea.save()
+        return redirect('startup')
+
+    # Retrieve and display submitted ideas for the logged-in entrepreneur
+    submitted_ideas = Idea.objects.filter(entrepreneur=request.user)  # Assuming you have a user field in the Idea model
+    return render(request, 'startup.html', {'submitted_ideas': submitted_ideas})
+
