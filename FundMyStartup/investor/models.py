05739d5fe0f89db56e2b450c01774053d016b660
maisha <maisha.jarin@gmail.com>
2023-08-20 13:46:08 +0600

    invested profile models

--- FundMyStartup/investor/models.py
+++ FundMyStartup/investor/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from entrepreneur.models import startup
+
 
 # Create your models here.
+class investedStartup(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    startup = models.ForeignKey(startup, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        verbose_name_plural = 'investedStartup'
--- FundMyStartup/investor/views.py
+++ FundMyStartup/investor/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 from entrepreneur.models import startup
 # Create your views here.
+from django.contrib.auth.decorators import login_required
+from .models import investedStartup
 
+
+
+@login_required(login_url='login')
 def investPro(request):
-    startups = startup.objects.all()
-    return render(request, 'investorProfile.html', {'startups':startups})
+    if request.method == 'POST':
+        user = request.user
+        startupst = request.POST['startup_id']
+        startupst = startup.objects.get(id=startupst)
+        amount = request.POST['amount']
+
+        invest = investedStartup.objects.create(user=user, startup=startupst, amount=amount)
+        invest.save()
+        print('invested')
+
+        return render(request, 'investorProfile.html')
+    else:
+        startups = startup.objects.all()
+        return render(request, 'investorProfile.html', {'startups':startups})
